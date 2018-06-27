ef7bc56890afd6630010e964e926a6405638e2a0
suhas4090 <35102923+suhas4090@users.noreply.github.com>
2018-06-27 10:07:11 +0530

    service name daily update

--- android/src/main/java/org/chimple/flores/sync/SyncUtils.java
+++ android/src/main/java/org/chimple/flores/sync/SyncUtils.java
@@ -24,7 +24,7 @@ import org.chimple.flores.sync.NSD.NSDSyncService;
 public class SyncUtils {
 
     public static final int HandShakeportToUse = 38777;
-    public static final String SERVICE_TYPE = "_MAUI_p2p_first._tcp";
+    public static final String SERVICE_TYPE = "_MAUI_p2p_second._tcp";
 
     public enum DiscoveryState {
         NONE,
