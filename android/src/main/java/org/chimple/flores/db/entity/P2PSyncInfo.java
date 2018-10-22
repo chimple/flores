90e0835f0a58c48e5dc70e2f56456e2908a2b4d5
Shyamal Upadhyaya <Shyamal.Upadhyaya@openlane.com>
2018-10-22 17:47:26 +0530

    bug fix

--- android/src/main/java/org/chimple/flores/db/entity/P2PSyncInfo.java
+++ android/src/main/java/org/chimple/flores/db/entity/P2PSyncInfo.java
@@ -5,7 +5,7 @@ import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.Index;
 import android.arch.persistence.room.PrimaryKey;
-
+import android.util.Log;
 import com.google.gson.annotations.Expose;
 
 import java.io.Serializable;
